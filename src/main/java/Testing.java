99ebc8d35e1025a1be3f78cfbd6487b2f02e42b3
Alekearl <alexandrhurko@gmail.com>
2020-11-04 14:07:57 +0200

    change one digit in int[][] matrix

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -1,5 +1,4 @@
 public class Testing {
-    public static int highest;
     public static void main(String[] args) {
         int[][] matrix = new int[][]{
                 {-10, -50, -150},
@@ -7,7 +6,6 @@ public class Testing {
                 {5, 25, 125, 1025}
         };
         ClassTest classTest = new ClassTest();
-        highest = classTest.highestInt(matrix);
+        int highest = classTest.highestInt(matrix);
     }
-    static int x = highest;
 }
